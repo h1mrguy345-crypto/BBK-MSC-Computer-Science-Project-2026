2c5ae09b565f4f30523cc5ce6829b88c3ddad76d
Makhail Rehman <h1mrguy345@gmail.com>
2026-05-13 10:08:31 +0100

    constructor for board

--- SnakesAndLaddersAnd/src/Board.java
+++ SnakesAndLaddersAnd/src/Board.java
@@ -1,2 +1,10 @@
 public class Board {
+    private int boardSize;
+    private Spot[] spot; // Spot class is to be made
+
+    //constructor for the board
+    public Board (int boardSize){
+        this.boardSize = boardSize;
+        this.spot = new Spot[boardSize + 1]; // increases the number of spots accordgingly to the board
+    }
 }
