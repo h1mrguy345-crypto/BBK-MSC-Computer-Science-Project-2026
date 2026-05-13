95e72daa838adf73cab9293591f8a82614c1bd88
Makhail Rehman <h1mrguy345@gmail.com>
2026-05-13 10:37:43 +0100

    spot constructor made, was meant to make playpos and mov private, not public

--- SnakesAndLaddersAnd/src/Spot.java
+++ SnakesAndLaddersAnd/src/Spot.java
@@ -1,6 +1,10 @@
 public class Spot {
-    public int playerPosition; //the spot of the board that a player currently is at
-    public int move; //the players movement, either advance or go back
+    private int playerPosition; //the spot of the board that a player currently is at
+    private int move; //the players movement, either advance or go back
 
+    public Spot(int playerPosition){
+        this.playerPosition = playerPosition;
+        this.move = 0;
+    }
 
 }
