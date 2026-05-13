d3bdfb19130e532950eab88cce814fec5e41564c
Makhail Rehman <h1mrguy345@gmail.com>
2026-05-13 10:23:40 +0100

    position and move for spot class

--- SnakesAndLaddersAnd/src/Spot.java
+++ SnakesAndLaddersAnd/src/Spot.java
@@ -1,2 +1,6 @@
 public class Spot {
+    public int playerPosition; //the spot of the board that a player currently is at
+    public int move; //the players movement, either advance or go back
+
+
 }
