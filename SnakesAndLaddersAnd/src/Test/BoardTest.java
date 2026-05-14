b6ff90ac09bb5ca30f9152b3a9bc7118a4b3ff9e
Makhail Rehman <h1mrguy345@gmail.com>
2026-05-14 12:59:27 +0100

    spot check test works

--- SnakesAndLaddersAnd/src/Test/BoardTest.java
+++ SnakesAndLaddersAnd/src/Test/BoardTest.java
@@ -1,12 +1,24 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 public class BoardTest {
+    //to confirm a board is created
     @Test
     public void setBoardConfirmed(){
         Board testBoard = new Board(15);
         Assertions.assertEquals(15, testBoard.getBoardSize());
         System.out.println("board is set correctly");
+    }
+
+    //to confirm the spots exist on the board
+    @Test
+    public void spotExist(){
+        Board testBoard = new Board(15);
+        assertNotNull(testBoard.getSpot()[1]);
+        assertNotNull(testBoard.getSpot()[15]);
+        System.out.println("there are spots on the board");
     }
 }
