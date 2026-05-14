cdd5fd0e206504ed0f1e6ba10fb56f213d02842c
Makhail Rehman <h1mrguy345@gmail.com>
2026-05-14 10:50:41 +0100

    in the midst of making a test to make sure setboard works, however need to install junit

--- SnakesAndLaddersAnd/src/Test/BoardTest.java
+++ SnakesAndLaddersAnd/src/Test/BoardTest.java
@@ -1,2 +1,9 @@
+import org.junit.jupiter.api.Test;
 public class BoardTest {
+    @Test
+    public void setBoardConfirmed(){
+        Board testBoard = new Board(15);
+        assertEquals(15, testBoard.getBoardSize());
+        System.out.println("board is set correctly");
+    }
 }
