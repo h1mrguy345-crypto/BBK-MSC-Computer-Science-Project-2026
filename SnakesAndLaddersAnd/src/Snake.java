b8151fd72ae649081659e8fbdd717dbcb6d501b6
Makhail Rehman <h1mrguy345@gmail.com>
2026-05-21 10:21:02 +0100

    snake constructor

--- SnakesAndLaddersAnd/src/Snake.java
+++ SnakesAndLaddersAnd/src/Snake.java
@@ -1,2 +1,21 @@
 public class Snake {
+    private int snakeTop;
+    private int snakeBottom;
+
+    /**
+     * constructor.
+     * if statement to ensure the snake does not act like a ladder
+     * */
+    public Snake(int snakeBottom, int snakeTop) {
+        try {
+            if (snakeBottom >= snakeTop) {
+                throw new IllegalArgumentException("You cannot to advance upwards with a snake");
+            }
+
+            this.snakeBottom = snakeBottom;
+            this.snakeTop = snakeTop;
+        } catch (IllegalArgumentException snakeError){
+            System.out.println(snakeError.getMessage());
+        }
+    }
 }
