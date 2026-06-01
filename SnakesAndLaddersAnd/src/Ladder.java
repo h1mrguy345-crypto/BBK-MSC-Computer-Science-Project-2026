550bfee2aff4fa0ddbf4ee193a4a699f4f247f19
Makhail Rehman <h1mrguy345@gmail.com>
2026-06-01 16:06:28 +0100

    copy pasted snakes class for ladder class, view javadoc of ladder

--- SnakesAndLaddersAnd/src/Ladder.java
+++ SnakesAndLaddersAnd/src/Ladder.java
@@ -1,2 +1,30 @@
 public class Ladder {
+    private int ladderTop;
+    private int ladderBottom;
+
+    /**
+     * constructor.
+     * if statement to ensure the ladder does not act like a snake
+     * 01/06/2026: this is essentially a copy paste of the snake class with adjustments made for the try statement
+     * */
+    public Ladder(int ladderBottomBottom, int ladderTop) {
+        try {
+            if (ladderBottomBottom <= ladderTop) {
+                throw new IllegalArgumentException("You cannot to advance dowmwards with a ladder");
+            }
+
+            this.ladderBottom = ladderBottom;
+            this.ladderTop = ladderTop;
+        } catch (IllegalArgumentException snakeError){
+            System.out.println(snakeError.getMessage());
+        }
+    }
+    public int getLadderTop(){
+        return ladderTop;
+    }
+
+    public int getLadderBottom() {
+        return ladderBottom;
+    }
 }
+
