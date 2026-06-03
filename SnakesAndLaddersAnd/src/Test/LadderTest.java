3db387a0be56e6a83f38a59243f3ace2224bbe7b
Makhail Rehman <h1mrguy345@gmail.com>
2026-06-03 21:57:20 +0100

    junit test to see if ladder works different to snake fails

--- SnakesAndLaddersAnd/src/Test/LadderTest.java
+++ SnakesAndLaddersAnd/src/Test/LadderTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class LadderTest {
+    //ladder should not make a player go downwards, aka not act like a snake
+    @Test
+    void ladderBehaviourNotSnakeBehaviour(){
+        assertThrows(IllegalArgumentException.ladderError,
+                ()-> new Ladder(50,15));
+    }
 }
