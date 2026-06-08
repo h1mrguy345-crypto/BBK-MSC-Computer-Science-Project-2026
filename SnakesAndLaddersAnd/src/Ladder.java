f3151af2df36e88e4aeffd264c5d05d58d2cd9bc
Makhail Rehman <h1mrguy345@gmail.com>
2026-06-08 11:05:46 +0100

    try and catch was causing error, now no message prints but ladder test passes

--- SnakesAndLaddersAnd/src/Ladder.java
+++ SnakesAndLaddersAnd/src/Ladder.java
@@ -8,16 +8,13 @@ public class Ladder {
      * 01/06/2026: this is essentially a copy paste of the snake class with adjustments made for the try statement
      * */
     public Ladder(int ladderBottom, int ladderTop) {
-        try {
             if (ladderBottom <= ladderTop) {
                 throw new IllegalArgumentException("You cannot to advance dowmwards with a ladder");
             }
 
             this.ladderBottom = ladderBottom;
             this.ladderTop = ladderTop;
-        } catch (IllegalArgumentException ladderError){
-            System.out.println(ladderError.getMessage());
-        }
+
     }
     public int getLadderTop(){
         return ladderTop;
