706933e19ca731e7c9c0be6feeafde12dde2f66a
Makhail Rehman <h1mrguy345@gmail.com>
2026-06-10 09:33:25 +0100

    ladder test now prints a message

--- SnakesAndLaddersAnd/src/Test/LadderTest.java
+++ SnakesAndLaddersAnd/src/Test/LadderTest.java
@@ -6,7 +6,9 @@ public class LadderTest {
     //ladder should not make a player go downwards, aka not act like a snake
     @Test
     void ladderBehaviourNotSnakeBehaviour(){
+        IllegalArgumentException LadError =
         assertThrows(IllegalArgumentException.class,
                 ()-> new Ladder(15,50));
+        System.out.println(LadError.getMessage());
     }
 }
