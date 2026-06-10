bdbb2fdd01052b52bd0ca543639efe3a6f55e906
Makhail Rehman <h1mrguy345@gmail.com>
2026-06-10 09:44:53 +0100

    removed try and catch from snake, as class is/should be identiacal to ladder

--- SnakesAndLaddersAnd/src/Snake.java
+++ SnakesAndLaddersAnd/src/Snake.java
@@ -7,16 +7,12 @@ public class Snake {
      * if statement to ensure the snake does not act like a ladder
      * */
     public Snake(int snakeBottom, int snakeTop) {
-        try {
             if (snakeBottom >= snakeTop) {
                 throw new IllegalArgumentException("You cannot to advance upwards with a snake");
             }
 
             this.snakeBottom = snakeBottom;
             this.snakeTop = snakeTop;
-        } catch (IllegalArgumentException snakeError){
-            System.out.println(snakeError.getMessage());
-        }
     }
     public int getSnakeTop(){
         return snakeTop;
