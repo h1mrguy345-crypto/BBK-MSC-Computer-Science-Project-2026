6b2f19a398bc52af1de53eab6534e5ba365854c0
Makhail Rehman <h1mrguy345@gmail.com>
2026-06-10 09:48:57 +0100

    snake test made to ensure it doesnt act like a ladder

--- SnakesAndLaddersAnd/src/Test/SnakeTest.java
+++ SnakesAndLaddersAnd/src/Test/SnakeTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class SnakeTest {
+   /**
+    * makes sure snakes don't bring you upwards
+    * same as ladder class, just adjusted to be for snake
+    * */
+    @Test
+    void SnakeBehaviourNotLadderBehaviour(){
+        IllegalArgumentException SError =
+                assertThrows(IllegalArgumentException.class,
+                        ()-> new Snake(50,15)); //same as ladder but because snakes take you down, you should not be able to go up
+        System.out.println(SError.getMessage());
+    }
 }
+
