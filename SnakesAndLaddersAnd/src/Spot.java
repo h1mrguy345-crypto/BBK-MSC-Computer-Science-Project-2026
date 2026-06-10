74aeccb8708d0649e17631ca64f0f743dba888f7
Makhail Rehman <h1mrguy345@gmail.com>
2026-06-10 12:19:10 +0100

    set and get ladder and snake for spot class

--- SnakesAndLaddersAnd/src/Spot.java
+++ SnakesAndLaddersAnd/src/Spot.java
@@ -1,6 +1,8 @@
 public class Spot {
     private int playerPosition; //the spot of the board that a player currently is at
     private int move; //the players movement, either advance or go back
+    private Snake snake;
+    private Ladder ladder;
 
     public Spot(int playerPosition){
         this.playerPosition = playerPosition;
@@ -24,5 +26,17 @@ public class Spot {
     }
     public int getMove(int move){
         return move;
+    }
+    public void setLadder(){
+        this.ladder = ladder;
+    }
+    public Ladder getLadder(){
+        return ladder;
+    }
+    public void setSnake(){
+        this.snake = snake;
+    }
+    public Snake getSnake(){
+        return snake;
     }
 }
