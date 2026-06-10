476d749ab7194992a0bd0593afbf8edaff70e977
Makhail Rehman <h1mrguy345@gmail.com>
2026-06-10 11:30:02 +0100

    copy paste test for ladder exists for snake with adjustment, see comment

--- SnakesAndLaddersAnd/src/Test/SnakeTest.java
+++ SnakesAndLaddersAnd/src/Test/SnakeTest.java
@@ -1,9 +1,25 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SnakeTest {
-   /**
+
+    /**
+     * copy paste of ladderexists test
+     * confirms snake exists on board
+     * 10/06/26: the top of the snake is tile 50 which is right,
+     * so i'm not sure what currently is causing the ladder to think 50 is the bottom
+     * */
+    @Test
+    void SnakeExists(){
+        Snake testSnake = new Snake(15,50);
+        System.out.println("snake exists");
+        assertEquals(15, testSnake.getSnakeBottom());
+        assertEquals(50, testSnake.getSnakeTop());
+    }
+
+    /**
     * makes sure snakes don't bring you upwards
     * same as ladder class, just adjusted to be for snake
     * */
