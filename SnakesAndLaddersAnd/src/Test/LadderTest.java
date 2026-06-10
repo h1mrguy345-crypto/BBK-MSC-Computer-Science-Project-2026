74743fc5baffa937cca7c0a3433ce3f5426e8aa8
Makhail Rehman <h1mrguy345@gmail.com>
2026-06-10 11:26:16 +0100

    ladder exists test

--- SnakesAndLaddersAnd/src/Test/LadderTest.java
+++ SnakesAndLaddersAnd/src/Test/LadderTest.java
@@ -1,8 +1,22 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LadderTest {
+
+    /**
+     * confirms ladder exists on board
+     * 10/06/26: apparently the bottom of the ladder is tile 50, should be the other way
+     * ^however will need to see how rest of the board works to see if this is fine
+     * */
+    @Test
+    void LadderExists(){
+        Ladder testLadder = new Ladder(50,15);
+        System.out.println("ladder exists");
+        assertEquals(50, testLadder.getLadderBottom());
+        assertEquals(15, testLadder.getLadderTop());
+    }
     //ladder should not make a player go downwards, aka not act like a snake
     @Test
     void ladderBehaviourNotSnakeBehaviour(){
