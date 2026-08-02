66d56112cdc03f58e42b579a3fcbc86714f98705
Makhail Rehman <h1mrguy345@gmail.com>
2026-08-02 23:24:01 +0100

    javadoc for dice

--- SnakesAndLaddersAnd/src/Dice.java
+++ SnakesAndLaddersAnd/src/Dice.java
@@ -1,9 +1,15 @@
+import java.util.Random;
+
 public class Dice {
+private Random dice;
 
     public Dice(){
-
+        dice = new Random();
     }
+/**
+ * manages the rolling of the dice (for either player)
+ * */
     public int DiceRoll(){
-        
+        return dice.nextInt();
     }
 }
