3e521764bb9a3b8b51d9bbebf55b0320b8f28561
Makhail Rehman <h1mrguy345@gmail.com>
2026-08-05 19:38:27 +0100

    valid dice roll test made

--- SnakesAndLaddersAnd/src/Test/DiceTest.java
+++ SnakesAndLaddersAnd/src/Test/DiceTest.java
@@ -1,2 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class DiceTest {
+    @Test
+    void rollExistsD6(){
+        Dice testDice = new Dice();
+        int testRoll = testDice.DiceRoll();
+        assertTrue(testRoll >= 1 && testRoll <= 6);
+        System.out.println("a number has been rolled between 1 and 6");
+    }
 }
