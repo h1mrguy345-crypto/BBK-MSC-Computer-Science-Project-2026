6d37aff87a58639783508727fdd6176f56b16145
Makhail Rehman <h1mrguy345@gmail.com>
2026-08-07 01:32:36 +0100

    main loops through array, but constantly has player 1 play rather than player 2

--- SnakesAndLaddersAnd/src/Main.java
+++ SnakesAndLaddersAnd/src/Main.java
@@ -43,11 +43,15 @@ public class Main{
         Dice mainDice = new Dice();
 
         while (!endGame){
+            Player player = activePlayer[activeUser];
+            System.out.println("current turn: " + player.getPlayerName());
         System.out.println("hit enter to roll the dice");
         playersInput.nextLine(); //rolls the dice
         int mainRoll = P1.playersRoll(mainDice); //to prevent automatic rolling, setting dice roll to players input (still automatically rolls as of right now)
         System.out.println("you have rolled " + mainRoll);
         P1.setPlayersCurrentPosition(P1.getPlayersCurrentPosition() + mainRoll);
         mainBoard.printBoard(P1,P2);}
+        //uses the array to switch to next player
+        activeUser = (activeUser + 1) % activePlayer.length;
     }
 }
