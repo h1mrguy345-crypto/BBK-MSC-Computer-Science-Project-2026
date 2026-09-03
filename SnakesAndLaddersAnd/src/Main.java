d6607d6e15d37cd55d5478a20a2bffe544359af5
Makhail Rehman <h1mrguy345@gmail.com>
2026-09-03 19:45:44 +0100

    commented message for examiners, explaining non-working state of the code

--- SnakesAndLaddersAnd/src/Main.java
+++ SnakesAndLaddersAnd/src/Main.java
@@ -1,7 +1,34 @@
 import java.util.Scanner;
 
 public class Main{
-//test, severe data issue, if this commit goes through, it confirms i can still commit
+/*
+* hi there, this is a note to examiners:
+* this is my MSC final project submission for Birkbeck College University of London (2026)
+* which consists of a playable version of classical snakes and ladders.
+* the original intention was to modify the game to include new rules and features (see report).
+*
+* the main thing i need to explain is the issue encountered when cloning.
+* long story short, i forgot to include the files (such as pom.xml) to the code to make the
+* game playable when running on a new computer/cloned.
+* when i tried to fix this, the result damaged the code so severely that the original refuses to load,
+* complaining about a maven rebuild failure, despite me removing maven.
+* i am sending this commit through the cloned copy that i made, which is why the code
+* highlights several issues regarding Junit.
+*
+* I want to ensure to everyone on the board that the game was fully playable and tested thoroughly
+* during these past few months by me. at the end of my report, you will see screenshots of a
+* working game where players are having their turns and moving across. you will also see several
+* screenshots of where I have ran tests and they have passed.
+* However, from the stress that the cloning has given me, as well as the remaining time. I will
+* no longer be touching the code any further, as my previous attempt nearly costed me losing the whole
+* code.
+*
+* I sincerely apologise for the lack of a now working copy, but I hope you can assess the project
+* for what the code demonstrates, as well as the report.
+* kind regards,
+* Makhail Rehman,
+* student at Birkbeck College, attending the MSC computer science course 2025-2026
+* */
     public static void main(){
         System.out.println("Welcome to 'Snakes and Ladders and...");
         Scanner UserChoice = new Scanner(System.in);
